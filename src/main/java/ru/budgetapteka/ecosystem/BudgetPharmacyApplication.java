194f60785b5f7736f6ee7c991bb6710663fe1c68
MaksRosenfeld <maks.duyko@gmail.com>
2022-05-16 13:07:36 +0200

    Initial commit

---
+++ src/main/java/ru/budgetapteka/ecosystem/BudgetPharmacyApplication.java
@@ -0,0 +1,13 @@
+package ru.budgetapteka.ecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetPharmacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/budgetapteka/ecosystem/BudgetPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.budgetapteka.ecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
